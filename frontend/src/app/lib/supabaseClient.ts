eadaaebe12f566aaddef0c16ce2c28886190b9f0
akhmadyasin <117156428+akhmadyasin@users.noreply.github.com>
2025-10-30 22:40:40 +0700

    Update supabaseClient.ts

--- frontend/src/app/lib/supabaseClient.ts
+++ frontend/src/app/lib/supabaseClient.ts
@@ -1,1 +1,12 @@
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Tambahkan ini:
+export function supabaseBrowser() {
+  return supabase;
+}
